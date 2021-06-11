bbef243662a00b414a973a152e75bbbc004a10d7
Thijs Smudde <thijssmudde@kateinnovations.com>
2021-06-11 23:40:27 +0200

    save

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const MAIN_HTML = path.join('file://', __dirname, 'main.html');
+const CHILD_PADDING = 50;
+
+const onAppReady = function () {
+  let parent = new BrowserWindow({
+    // center: true,
+    x: 0,
+    y: 0,
+    width: 700,
+    height: 700,
+    transparent: true,
+    frame: false,
+    fullscreenable: false,
+    simpleFullscreen: true
+  });
+
+  parent.setIgnoreMouseEvents(true)
+  parent.setFocusable(false)
+  parent.setAlwaysOnTop(true, 'status')
+  parent.maximize()
+
+  parent.once('close', () => {
+    parent = null;
+  });
+
+  parent.loadURL(MAIN_HTML);
+};
+
+//~ app.on('ready', onAppReady);
+app.on('ready', () => setTimeout(onAppReady, 500));
